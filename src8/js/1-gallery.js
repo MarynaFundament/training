01b1b95ad4459ee03c5ad58e2d6aad9c7a047d1c
MarynaFundament <marina.fundament@ukr.net>
2024-02-25 18:23:46 +0200

    sdcd

---
+++ src8/js/1-gallery.js
@@ -0,0 +1,37 @@
+import { items } from './gallery-items.js';
+console.log(items)
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const ul = document.querySelector(".gallery")
+
+ul.style.listStyle = "none";
+ul.style.display = "flex";
+ul.style.flexWrap = "wrap";
+ul.style.gap = "24px"; 
+ul.style.marginLeft = "150px";
+
+
+  ul.innerHTML = items.reduce((html, {preview,original,description }) => 
+  html + `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      alt="${description}"
+      style= "width:360px ", "height:"200px"
+    />
+  </a>
+</li>`
+, "")
+
+
+let lightbox = new SimpleLightbox('.gallery a',
+ { 
+  
+    captionsData: 'alt', 
+    captionDelay: 1050 
+});
+
